35b4e73c266dbd69f84816747eada03f52b38a2d
Andrei Menin <andreimenin@gmail.com>
2022-11-09 13:36:25 -0300

    02 Widgets - 04 - Column e Row Stack->os elementos ficam um em cima do outro, sequencialmente o maior fica em primeiro; alinhamento dos elementos -> AlignmentDirectional
    Column->alinhamento dos elementos-> MainAxisAlignment -> alinha os itens na vertical; CrossAxisAlignment -> alinha os itens na horizontal;
    
    Row->alinhamento dos elementos-> MainAxisAlignment -> alinha os itens na horizontal; CrossAxisAlignment -> alinha os itens na vertical;

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,35 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Stack(
-          alignment: AlignmentDirectional.center,
-          children: [
-        Container(color: Colors.red, width: 100, height: 100,),
-        Container(color: Colors.blue, width: 50, height: 50)
-      ]),
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(color: Colors.redAccent, height: 100, width: 100,),
+              Container(color: Colors.blueAccent, height: 50, width: 50,),
+            ],
+          ),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(color: Colors.blueAccent, height: 100, width: 100,),
+              Container(color: Colors.redAccent, height: 50, width: 50,),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(color: Colors.orange, height: 50, width: 50,),
+              Container(color: Colors.pinkAccent, height: 50, width: 50,),
+              Container(color: Colors.purple, height: 50, width: 50,),
+            ]
+          )
+        ]
+      ),
     );
   }
 }
