ef8ab98a9ab43d8bd9362e976344bb407b58942e
Andrei Menin <andreimenin@gmail.com>
2022-11-20 03:28:19 -0300

    03 Stateless vs Stateful - 09 Stateful Widget e setState *alterando o Stateless (widget estático) para o Stateful, para poder re renderizar de acordo com a ação do clique do botão *setState para obervar o valor de 'nivel' alterado e re-renderizar o texto na tela *variavel nivel fora do override para poder evitar que seu valor seja resetado ao re-renderizar o widget

--- lib/main.dart
+++ lib/main.dart
@@ -31,9 +31,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String nome;
+
   const Task(this.nome, {Key? key}) : super(key: key);
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -43,27 +51,42 @@ class Task extends StatelessWidget {
         child: Stack(
           children: [
             Container(color: Colors.blue, height: 140),
-            Container(
-              color: Colors.white,
-              height: 100,
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      color: Colors.black26,
-                      width: 72,
-                      height: 100,
-                    ),
-                    Container(
-                      width: 200,
-                        child: Text(nome, style: TextStyle(
-                            fontSize: 24,
-                            overflow: TextOverflow.ellipsis
-                          ),
-                        )),
-                    ElevatedButton(
-                        onPressed: () {}, child: Icon(Icons.arrow_drop_up))
-                  ]),
+            Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          color: Colors.black26,
+                          width: 72,
+                          height: 100,
+                        ),
+                        Container(
+                            width: 200,
+                            child: Text(
+                              widget.nome,
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  overflow: TextOverflow.ellipsis),
+                            )),
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {//observando o valor da variavel nivel para re-renderizar a tela
+                                nivel++;
+                              });
+                              print(nivel);
+                            },
+                            child: Icon(Icons.arrow_drop_up))
+                      ]),
+                ),
+                Text(
+                  'Nível: $nivel',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                )
+              ],
             ),
           ],
         ),
