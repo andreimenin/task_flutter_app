44404b4603a79279e6dd703fa6ec887a0ff4cad7
Andrei Menin <andreimenin@gmail.com>
2022-11-20 03:01:23 -0300

    03 Stateless vs Stateful - 07 ListView e padding *List view é um componente que torna possível utilizar o scroll na tela *Padding é o componente que tem propriedade padding semalhante ao css *Alterações no text

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,11 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         //estrutura pronta de layout
         appBar: AppBar(title: Text('Tarefas')),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: ListView(
           children: [
             Task('Aprender Flutter'),
             Task('Dirigir'),
-            Task('Meditar'),
+            Task('Meditar na praia enquanto ouve as ondas do mar'),
           ],
         ),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
@@ -38,27 +37,36 @@ class Task extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          Container(color: Colors.blue, height: 140),
-          Container(
-            color: Colors.white,
-            height: 100,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    color: Colors.black26,
-                    width: 72,
-                    height: 100,
-                  ),
-                  Text(nome),
-                  ElevatedButton(
-                      onPressed: () {}, child: Icon(Icons.arrow_drop_up))
-                ]),
-          ),
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(color: Colors.blue, height: 140),
+            Container(
+              color: Colors.white,
+              height: 100,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      color: Colors.black26,
+                      width: 72,
+                      height: 100,
+                    ),
+                    Container(
+                      width: 200,
+                        child: Text(nome, style: TextStyle(
+                            fontSize: 24,
+                            overflow: TextOverflow.ellipsis
+                          ),
+                        )),
+                    ElevatedButton(
+                        onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+                  ]),
+            ),
+          ],
+        ),
       ),
     );
   }
