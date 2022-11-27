92a380bd1ca809cb42bc0bd8cb65afc8318d2372
Andrei Menin <andreimenin@gmail.com>
2022-11-27 16:26:23 -0300

    05 Boas práticas - 08 Assets *configurando o assets (recursos externos) do projeto no arquivo pubspec.yaml *clicando em seguida no botão 'Pub get' para atualizar a alteração do assets *trocando o Image.network por Image.asset para referenciar as imagens dentro do projeto

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -16,30 +16,22 @@ class _InitialScreenState extends State<InitialScreen> {
     return Scaffold(
       //estrutura pronta de layout
       appBar: AppBar(
-          leading: Container(color: Colors.black26), title: const Text('Tarefas')),
+          leading: Container(color: Colors.black26),
+          title: const Text('Tarefas')),
       body: AnimatedOpacity(
         opacity: opacidade ? 1 : 0,
         duration: const Duration(milliseconds: 800),
         child: ListView(
           children: const [
-            Task(
-                'Aprender Flutter',
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                3),
-            Task(
-                'Andar de Bike',
-                'https://thumbs.dreamstime.com/b/woman-mountain-bike-riding-ridge-carpathian-mountains-40195258.jpg',
-                2),
-            Task(
-                'Meditar na praia enquanto ouve as ondas do mar',
-                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
-                5),
-            Task('Ler',
-                'https://thumbs.dreamstime.com/b/book-to-read-142078.jpg', 4),
-            Task(
-                'Jogar',
-                'https://thumbs.dreamstime.com/z/playing-video-game-close-up-child-hands-th-late-night-54233429.jpg',
-                0),
+            Task('Aprender Flutter', 'assets/images/flutter.png', 3),
+            Task('Andar de Bike', 'assets/images/bike.jpg', 2),
+            Task('Meditar na praia enquanto ouve as ondas do mar',
+                'assets/images/meditation.jpeg', 5),
+            Task('Ler', 'assets/images/book.jpg', 4),
+            Task('Jogar', 'assets/images/playgame.jpg', 0),
+            SizedBox(
+              height: 80,
+            ),
           ],
         ),
       ),
