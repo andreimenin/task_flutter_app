d83588479662fd6ed181cf83580552770d4b8ad3
Andrei Menin <andreimenin@gmail.com>
2022-11-28 04:25:50 -0300

    01 Formulário - 03 Como será nosso formulário?

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_flutter_app/screens/form_screen.dart';
 import 'package:task_flutter_app/screens/initial_screen.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InitialScreen(),
+      home: const FormScreen(),
     );
   }
 }
