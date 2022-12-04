f0d7d210d3b6992935e6a459e7febe4259d49d09
Andrei Camargo Menin <andrei@glitzi.com.mx>
2022-12-04 03:45:45 -0300

    02 Lidando com dados - 03 Estrutura de dados

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -7,6 +7,15 @@ Future<Database> getDatabase() async {
   //task.db é o arquivo de banco de dados que será criado
   final String path = join(await getDatabasesPath(), 'task.db'); //await -> esperando o getDatabasesPath() ser encontrado
   return openDatabase(path, onCreate: (db, version) {
-    db.execute(tabela)
+    db.execute(tableSql);
   }, version: 1,);
 }
+const String tableSql = 'CREATE TABLE $_tablename('
+    '$_name TEXT, '
+    '$_difficulty INTEGER, '
+    '$_image TEXT)';
+
+const String _tablename = 'taskTable';
+const String _name = 'name';
+const String _difficulty = 'difficulty';
+const String _image = 'image';
