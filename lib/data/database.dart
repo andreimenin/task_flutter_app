34861749f76d677bfadc00ba358bca1bbddfb1a1
Andrei Camargo Menin <andrei@glitzi.com.mx>
2022-12-04 12:24:38 -0300

    03 CRUD: Ler arquivos - 04 Sobre Read criação da função findAll para chamar a requisição de listagem do banco de dados

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -2,8 +2,8 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:task_flutter_app/data/task_dao.dart';
 
-//async acontece enquanto outras funcionalidades acontecem ao mesmo tempo
-Future<Database> getDatabase() async {
+//função responsável por achar o caminho do banco de dados, se ele não existir, será criado
+Future<Database> getDatabase() async {//async acontece enquanto outras funcionalidades acontecem ao mesmo tempo
   //buscando pelo caminho perfeito do dispositivo para buscar arquivos
   //task.db é o arquivo de banco de dados que será criado
   final String path = join(await getDatabasesPath(), 'task.db'); //await -> esperando o getDatabasesPath() ser encontrado
--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,5 +1,7 @@
 //classe responsável por qualquer comunicação com o banco de dados
+import 'package:sqflite/sqflite.dart';
 import 'package:task_flutter_app/components/task.dart';
+import 'package:task_flutter_app/data/database.dart';
 
 class TaskDao {
   //utilizando o static para sempre ter esses mesmos valores quando a classe ser instanciada
@@ -14,8 +16,15 @@ class TaskDao {
   static String _image = 'image';
 
   save(Task tarefa) async {}
-  //Future utilizado para requisições de carregamento
-  Future<List<Task>> findAll() async {}
+  //função de read (listagem)
+  Future<List<Task>> findAll() async {
+    //Future utilizado para chamar requisições de carregamento de dados
+    print('Acessando o findAll: ');
+    final Database bancoDeDados = await getDatabase(); //o await significa que irá esperar fazer a requisição da função Future
+    final List<Map<String, dynamic>> result = await bancoDeDados.query(_tablename);
+    print('Procurando dados no banco de dados... encontrado: $result');
+    return toList();
+  }
   Future<List<Task>> find(String nomeDaTarefa) async {}
   delete(String nomeDaTarefa) async {}
 }
