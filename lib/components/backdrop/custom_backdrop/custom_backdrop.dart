91b0d4e9c97d4ad8263cc75e39a27b4074418de9
Andrei Camargo Menin <andrei@glitzi.com.mx>
2023-03-12 20:24:13 -0300

    Custom Backdrop - use DraggableScrollableSheet Only will close the backdrop when drag to the final of the screen DraggableScrollableSheet to the content of bottomSheet be draggable when it's with size full height

--- lib/components/backdrop/custom_backdrop/custom_backdrop.dart
+++ lib/components/backdrop/custom_backdrop/custom_backdrop.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'dart:ui';
 
 class CustomBackdrop {
-  // Widget customdata;
-  // CustomBackdrop(this.customdata);
   /*
   * Backdrop
   * @return void
@@ -11,55 +11,131 @@ class CustomBackdrop {
   double defaultBorderRadius = 8.0;
   void bottomSheet(BuildContext contextParent, Widget customdata,
       {bool showPan = true}) {
+    
+    late final GlobalKey globalKeyColumn = GlobalKey();
+    
+    double parentHeight = 0;
+    double childHeight = 0.0001;
+    bool isScrollEnd = false;
+
+    double maxExtent = 0;
+    double extent = 0;
+
+    DraggableScrollableController dragControl = DraggableScrollableController();
+
     Future<void> bottomSheetAwaitClose = showModalBottomSheet<void>(
-        enableDrag: true,
         isDismissible: true,
         useRootNavigator: true,
+        enableDrag: true,
         isScrollControlled: true,
-        shape: RoundedRectangleBorder(
-          borderRadius:
-          BorderRadius.only(
-            topLeft: Radius.circular(defaultBorderRadius),
-            topRight: Radius.circular(defaultBorderRadius),
-          ),
-        ),
+        backgroundColor: Colors.transparent,
         context: contextParent,
-        builder: (BuildContext context) => ConstrainedBox(
-          constraints: BoxConstraints(
-            maxHeight: MediaQuery.of(contextParent).size.height - MediaQuery.of(contextParent).viewPadding.top,
-          ),
-          child: Padding(
-                padding: EdgeInsets.only(
-              bottom: MediaQuery.of(contextParent).viewInsets.bottom),
-                child: 
-                    Stack(
-                      children: [
-                        SizedBox(
-                          height: 30.0,
-                          child: showPan ? botomSheetPan() : const SizedBox(),
+          builder: (BuildContext contextBuilder) {
+          return StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+            double headerHeight = 30;
+            MediaQueryData mediaQueryData = MediaQuery.of(context);
+
+            double heightOfDevice = mediaQueryData.size.height;
+            double notificationBarSize = MediaQueryData.fromWindow(window).padding.top;
+            double paddingBottom = mediaQueryData.viewInsets.bottom;
+
+            SchedulerBinding.instance.addPostFrameCallback((_) async {
+              // ignore: prefer_typing_uninitialized_variables
+              var renderBox;
+              if (globalKeyColumn.currentContext?.findRenderObject() != null) {
+                renderBox = globalKeyColumn.currentContext!.findRenderObject() as RenderBox;
+              }
+              setState((){
+                parentHeight = (renderBox?.size.height + headerHeight + paddingBottom);
+              });
+              
+              if(isScrollEnd && (extent - ((headerHeight + paddingBottom) /heightOfDevice)) > childHeight){
+                await dragControl.animateTo(
+                    maxExtent,
+                    curve: Curves.easeOut,
+                    duration: Duration(milliseconds: (parentHeight/7).round()),
+                );
+              }
+              else if(isScrollEnd && (double.parse(dragControl.size.toStringAsFixed(1))  == extent)){
+                Navigator.of(contextBuilder).maybePop();
+              }
+            });
+
+            return ConstrainedBox(
+              constraints: BoxConstraints(maxHeight: heightOfDevice - notificationBarSize),
+              child: AnimatedContainer(
+                //curve: Curves.bounceIn,
+                height: parentHeight,
+                duration: Duration(milliseconds: (parentHeight/7).round()),
+                child: NotificationListener<DraggableScrollableNotification>(
+                
+                  onNotification: (notification) {
+                    //identify drag down to not stop and close the bottomSheet directly
+                    maxExtent = notification.maxExtent;
+                    extent = double.parse(notification.extent.toStringAsFixed(1));
+                    
+                    return true;
+                  },
+                  child: DraggableScrollableSheet(
+                    controller: dragControl,
+                    initialChildSize: 1,
+                    maxChildSize: 1,
+                    minChildSize: childHeight,
+                    expand: true,
+                    builder: (context, scrollController) {
+                    return
+                      Container(
+                        decoration: BoxDecoration(color: Colors.white,
+                          borderRadius:
+                          BorderRadius.only(
+                            topLeft: Radius.circular(defaultBorderRadius),
+                            topRight: Radius.circular(defaultBorderRadius),
+                          ),),
+                        child: Padding(
+                          padding: EdgeInsets.only(bottom: paddingBottom),
+                          child: Stack(
+                            children: [
+                              SizedBox(
+                                height: headerHeight,
+                                child: showPan ? botomSheetPan() : const SizedBox(),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 30),
+                                child: NotificationListener<ScrollNotification>(
+                                  onNotification: (notification){
+                                    if (notification is ScrollStartNotification) {
+                                     
+                                        isScrollEnd = false;
+                                    } else if (notification is ScrollUpdateNotification) {
+                                        isScrollEnd = false;
+                                    } else if (notification is ScrollEndNotification) {
+                                        isScrollEnd = true;
+                                    }
+                                    return true;
+                                  },
+                                  child: SingleChildScrollView(
+                                  controller: scrollController,
+                                  child: Column(
+                                    key: globalKeyColumn,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      customdata,
+                                      const SizedBox(
+                                        height: 20,
+                                      ),
+                                    ],
+                                  ),),
+                                ))]),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 30),
-                          child: SingleChildScrollView(
-                                    child: Wrap(
-                                      children: [
-                                        Column(
-                                          mainAxisAlignment: MainAxisAlignment.start,
-                                          children: <Widget>[
-                                            customdata,
-                                            const SizedBox(
-                                              height: 40,
-                                            ),
-                                          ],
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                )],
-                    )
-                          ,
+                      );
+                    }),
+                ),
               ),
-        ));
+            );
+            });
+          },
+        );
 
     bottomSheetAwaitClose.then((void value) => {});
   }
